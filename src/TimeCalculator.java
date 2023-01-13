e55ee91882a5e94466b6be265943d65fcc86bbeb
Junhici <lucasmegaX0501@gmaill.com>
2023-01-13 22:07:09 +0100

    Commit TextTimer and TimeCalculator

--- src/TimeCalculator.java
+++ src/TimeCalculator.java
@@ -14,25 +14,29 @@ public class TimeCalculator {
             this.WPM = WPM;
         }
     }
+
+    // Set the calculator WPM (Word per Minute)
     void setWpm(TimeCalculaterWPM newWpm){
         this.wpm = newWpm;
     }
+    // Set calculator text
     void setText(String newText){
         this.text = newText;
     }
+    // Calculate how much seconds you need to read the text
     float calculateTime(){
         String[] words = this.text.split(" ");
         System.out.println(words.length);
         float nWords = words.length;
         return nWords / this.wpm.WPM * 60;
     }
-
+    // Change the time
     void setTime(int hour, int minute, int second){
         this.h = hour;
         this.m = minute;
         this.s = second;
     }
-
+    // Decrease the time by one second
     boolean secondStep(){
         switch(this.s){
             case 0:
